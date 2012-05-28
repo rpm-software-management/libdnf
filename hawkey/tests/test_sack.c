d8f5d432cbc3a6f166a473f10fbf2c64d1b45cd9
Ales Kozumplik <ales@redhat.com>
2012-05-28 15:11:18 +0200

    testing repo: add presto metadata.

--- hawkey/tests/test_sack.c
+++ hawkey/tests/test_sack.c
@@ -144,7 +144,7 @@ START_TEST(test_filelist_from_cache)
     /* test a file that isn't matched by repodata_filelistfilter_matches() in
        libsolv */
     dataiterator_init(&di, pool, 0, 0, SOLVABLE_FILELIST,
-		      "/usr/lib/python2.7/site-packages/today.pyc",
+		      "/usr/lib/python2.7/site-packages/tour/today.pyc",
 		      SEARCH_STRING | SEARCH_FILES | SEARCH_COMPLETE_FILELIST);
     for (count = 0; dataiterator_step(&di); ++count) ;
     fail_unless(count == 1);
