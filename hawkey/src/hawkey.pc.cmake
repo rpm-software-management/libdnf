06e53d7c95f7073992fd7257baba45b2ee98335f
Scott Tsai <scottt.tw@gmail.com>
2012-06-26 16:51:44 +0800

    Add hawkey.pc for use with pkgconfig

---
+++ hawkey/src/hawkey.pc.cmake
@@ -0,0 +1,11 @@
+prefix=@CMAKE_INSTALL_PREFIX@
+libdir=@LIB_INSTALL_DIR@
+includedir=@CMAKE_INSTALL_PREFIX@/include
+
+Name: hawkey
+Description: Resolves dependencies based on RPMDB and yum repos.
+Version: @VERSION@
+Requires.private: rpm zlib
+Libs: -L${libdir} -lhawkey -lsolv -lsolvext
+Libs.private: -lexpat
+Cflags: -I${includedir} -D_FILE_OFFSET_BITS=64
