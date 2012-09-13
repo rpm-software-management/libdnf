e6f1412416d886293bd510a98582c8fb4feb663d
Ales Kozumplik <ales@redhat.com>
2012-09-13 14:33:52 +0200

    cosmetic: doc: copyright.

--- hawkey/doc/conf.py
+++ hawkey/doc/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Hawkey'
-copyright = u'2012, Aleš Kozumplík'
+copyright = u'2012, Red Hat'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
