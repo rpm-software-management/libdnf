7f173e02372b90ff7d5de596262ae6fdafbfc389
Ales Kozumplik <ales@redhat.com>
2012-09-25 15:24:03 +0200

    doc: move the hawkey reference to man section 3.

--- hawkey/doc/conf.py
+++ hawkey/doc/conf.py
@@ -215,7 +215,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'hawkey', u'Hawkey Documentation',
-     [u'Aleš Kozumplík'], 1)
+     [u'Aleš Kozumplík'], 3)
 ]
 
 # If true, show URL addresses after external links.
