83179be3e638b61e5b25e9966012f1a6b22dd724
Ales Kozumplik <ales@redhat.com>
2012-10-02 14:21:10 +0200

    tests: py: ensure Package hashability works as expected.

--- hawkey/tests/python/tests/test_package.py
+++ hawkey/tests/python/tests/test_package.py
@@ -17,6 +17,26 @@ class Package(base.TestCase):
         self.assertEqual(str(self.pkg),
                          "flying-2-9.noarch")
 
+    def test_hashability(self):
+        """ Test that hawkey.Package objects are hashable.
+
+            So they can be keys in a dict.
+        """
+        d = {}
+        d[self.pkg] = True
+        pkg2 = hawkey.Query(self.sack).filter(name__eq="fool").run()[0]
+        d[pkg2] = 1
+        self.assertEqual(len(d), 2)
+
+        # if we get the same package twice it is a different object (hawkey does
+        # not pool Packages at this time). It however compares equal.
+        pkg3 = hawkey.Query(self.sack).filter(name__eq="fool").run()[0]
+        self.assertIsNot(pkg2, pkg3)
+        self.assertEquals(pkg2, pkg3)
+        d[pkg3] += 1
+        self.assertEqual(d[pkg2], 2)
+        self.assertEqual(len(d), 2)
+
 class WithYumRepo(base.TestCase):
     def setUp(self):
         self.sack = hawkey.test.TestSack(repo_dir=self.repo_dir)
