7b0574ced1391f1682c3d3cc3df180e43c9f37a4
Ales Kozumplik <ales@redhat.com>
2012-10-16 17:36:29 +0200

    errno: use enum for the values.
    Order sequences of related error codes by decreasing specificity (nothing too
    formal).

--- hawkey/src/errno.h
+++ hawkey/src/errno.h
@@ -5,17 +5,18 @@
 extern "C" {
 #endif
 
-#define HY_E_FAILED		1	// general runtime error
-#define HY_E_IO			2	// I/O error
-#define HY_E_CACHE_WRITE	3	// cache write error
-#define HY_E_QUERY		4	// ill-formed query
-#define HY_E_ARCH		5	// unknown arch
-#define HY_E_VALIDATION		6	// validation check failed
-#define HY_E_LIBSOLV		7	// erorr propagated from libsolv
-#define HY_E_SELECTOR		8	// ill-specified selector
-#define HY_E_OP			9	// client programming error
-#define HY_E_NO_SOLUTION	10	// goal found no solutions
-
+enum _hy_errors_e {
+    HY_E_FAILED		= 1,	// general runtime error
+    HY_E_OP,			// client programming error
+    HY_E_LIBSOLV,		// erorr propagated from libsolv
+    HY_E_IO,			// I/O error
+    HY_E_CACHE_WRITE,		// cache write error
+    HY_E_QUERY,			// ill-formed query
+    HY_E_ARCH,			// unknown arch
+    HY_E_VALIDATION,		// validation check failed
+    HY_E_SELECTOR,		// ill-specified selector
+    HY_E_NO_SOLUTION,		// goal found no solutions
+};
 
 extern __thread int hy_errno;
 
