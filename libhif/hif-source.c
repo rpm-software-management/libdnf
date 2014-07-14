3caa009b9f6dd617602a421bb10d0e7315a8e38a
Colin Walters <walters@verbum.org>
2014-07-14 09:09:51 -0400

    source: Only set librepo option if value is set
    librepo right now has some magical (and deprecated) handling of the
    LRO_MIRRORLIST option; for one thing, it calls strstr() on the url and
    if it contains "metalink", it also sets it as a LRO_METALINKURL.
    
    (Note the default Fedora repo files have mirrorlist=.../metalink/...)
    
    But libhif circumvents this by explicitly setting NULL on the handle
    even if the yum repository file didn't have it.  Change the setters to
    skip setting the options to NULL, leaving them as default.
    
    This fixes rpm-ostree composes.

--- libhif/hif-source.c
+++ libhif/hif-source.c
@@ -916,17 +916,17 @@ hif_source_set_keyfile_data (HifSource *source, GError **error)
 
 	/* baseurl is optional */
 	baseurls = g_key_file_get_string_list (priv->keyfile, priv->id, "baseurl", NULL, NULL);
-	if (!lr_handle_setopt (priv->repo_handle, error, LRO_URLS, baseurls))
+	if (baseurls && !lr_handle_setopt (priv->repo_handle, error, LRO_URLS, baseurls))
 		return FALSE;
 
 	/* mirrorlist is optional */
 	mirrorlist = g_key_file_get_string (priv->keyfile, priv->id, "mirrorlist", NULL);
-	if (!lr_handle_setopt (priv->repo_handle, error, LRO_MIRRORLIST, mirrorlist))
+	if (mirrorlist && !lr_handle_setopt (priv->repo_handle, error, LRO_MIRRORLIST, mirrorlist))
 		return FALSE;
 
 	/* metalink is optional */
 	metalink = g_key_file_get_string (priv->keyfile, priv->id, "metalink", NULL);
-	if (!lr_handle_setopt (priv->repo_handle, error, LRO_METALINKURL, metalink))
+	if (metalink && !lr_handle_setopt (priv->repo_handle, error, LRO_METALINKURL, metalink))
 		return FALSE;
 
 	/* gpgcheck is optional */
