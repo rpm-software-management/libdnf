1e03e4bb7d22bc376b37165bec8870a63ecfe7ed
Richard Hughes <richard@hughsie.com>
2014-08-29 11:37:46 +0100

    Fix a logic error to fix refreshing with HIF_SOURCE_UPDATE_FLAG_FORCE

--- libhif/hif-source.c
+++ libhif/hif-source.c
@@ -1085,7 +1085,7 @@ hif_source_update (HifSource *source,
 			     error_local->message);
 		goto out;
 	}
-	if ((flags & HIF_SOURCE_UPDATE_FLAG_FORCE) == 0 ||
+	if ((flags & HIF_SOURCE_UPDATE_FLAG_FORCE) == 0 &&
 	    timestamp_new < priv->timestamp_generated) {
 		g_debug ("fresh metadata was older than what we have, ignoring");
 		if (!hif_state_finished (state, error))
