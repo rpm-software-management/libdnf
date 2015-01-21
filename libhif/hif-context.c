rpm-software-management/libdnf

commit c17515c0a5e33b81ee15fdfbdce38930f91a6631

Don't use hif_context_get_transaction() from multiple threads
I was told that librpm is safe to use from multiple threads when more than one
thread doesn't try to access the database at the same time.

It seems this condition isn't true when different threads try to use the same
rpmts which can result in db3 index corruption. To fix this, add a warning to
hif_context_get_transaction() asking it not to be used when using multiple
threads, and print a warning if this is detected.

libhif/hif-context.c
@@ -66,6 +66,7 @@ struct _HifContextPrivate
 	gboolean		 keep_cache;
 	HifLock			*lock;
 	HifTransaction		*transaction;
+	GThread			*transaction_thread;
 	GFileMonitor		*monitor_rpmdb;
 	GHashTable              *override_macros;
 
@@ -387,10 +388,38 @@ hif_context_get_sources (HifContext *context)
 }
 
 /**
+ * hif_context_ensure_transaction:
+ **/
+static void
+hif_context_ensure_transaction (HifContext *context)
+{
+	HifContextPrivate *priv = GET_PRIVATE (context);
+
+	/* create if not yet created */
+	if (priv->transaction == NULL) {
+		priv->transaction = hif_transaction_new (context);
+		priv->transaction_thread = g_thread_self ();
+		hif_transaction_set_sources (priv->transaction, priv->sources);
+		hif_transaction_set_flags (priv->transaction,
+					   HIF_TRANSACTION_FLAG_ONLY_TRUSTED);
+		return;
+	}
+
+	/* check the transaction is not being used from a different thread */
+	if (priv->transaction_thread != g_thread_self ())
+		g_warning ("transaction being re-used by a different thread!");
+}
+
+/**
  * hif_context_get_transaction:
  * @context: a #HifContext instance.
  *
  * Gets the transaction used by the transaction.
+ *
+ * IMPORTANT: This function cannot be used if #HifContext is being re-used by
+ * different threads, even if threads are run one at a time. If you're doing
+ * this, just create a HifTransaction for each thread rather than using the
+ * context version.
  *
  * Returns: (transfer none): the HifTransaction object
  *
@@ -400,6 +429,8 @@ HifTransaction *
 hif_context_get_transaction (HifContext *context)
 {
 	HifContextPrivate *priv = GET_PRIVATE (context);
+	/* ensure transaction exists */
+	hif_context_ensure_transaction (context);
 	return priv->transaction;
 }
 
@@ -1278,8 +1309,6 @@ hif_context_setup (HifContext *context,
 	priv->sources = hif_repos_get_sources (priv->repos, error);
 	if (priv->sources == NULL)
 		return FALSE;
-	priv->transaction = hif_transaction_new (context);
-	hif_transaction_set_sources (priv->transaction, priv->sources);
 
 	/* setup a file monitor on the rpmdb, if we're operating on the native / */
 	if (g_strcmp0 (priv->install_root, "/") != 0) {
@@ -1322,6 +1351,9 @@ hif_context_run (HifContext *context, GCancellable *cancellable, GError **error)
 	HifContextPrivate *priv = GET_PRIVATE (context);
 	HifState *state_local;
 	gboolean ret;
+
+	/* ensure transaction exists */
+	hif_context_ensure_transaction (context);
 
 	/* connect if set */
 	hif_state_reset (priv->state);
@@ -1639,6 +1671,9 @@ gboolean
 hif_context_commit (HifContext *context, HifState *state, GError **error)
 {
 	HifContextPrivate *priv = GET_PRIVATE (context);
+
+	/* ensure transaction exists */
+	hif_context_ensure_transaction (context);
 
 	/* run the transaction */
 	return hif_transaction_commit (priv->transaction,

commit b45e805b04d5bb5ede5bdd7e54a58d83909a9fa5

context: Reinstate default of not using ONLY_TRUSTED
Regression from c17515c0a5e33b81ee15fdfbdce38930f91a6631

Commit 5fbf26f900f2430b345636d29692b7bac20f91f6 changed things so that
we didn't use HIF_TRANSACTION_FLAG_ONLY_TRUSTED by default (clients
could set it).

The thread safety commit above apparently accidentally re-introduced
it.

libhif/hif-context.c
@@ -400,8 +400,6 @@ hif_context_ensure_transaction (HifContext *context)
 		priv->transaction = hif_transaction_new (context);
 		priv->transaction_thread = g_thread_self ();
 		hif_transaction_set_sources (priv->transaction, priv->sources);
-		hif_transaction_set_flags (priv->transaction,
-					   HIF_TRANSACTION_FLAG_ONLY_TRUSTED);
 		return;
 	}
 