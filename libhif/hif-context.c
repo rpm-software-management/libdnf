7b4cb4ee996fa61a739399f5042851157806c7b9
Colin Walters <walters@verbum.org>
2016-04-27 22:37:07 -0400

    context: Add a globals_init API
    rpm-ostree wants to invoke _get_base_arch before we've called the full
    `setup()`, since that wants an install root which we don't have yet.
    
    Adding this public API allows rpm-ostree to still get a GError from
    the rc read, and then invoking the error-free
    `hif_context_get_base_arch()`.
    
    Signed-off-by: Igor Gnatenko <ignatenko@redhat.com>

--- libhif/hif-context.c
+++ libhif/hif-context.c
@@ -151,6 +151,45 @@ hif_context_init(HifContext *context)
     priv->cache_age = 60 * 60 * 24 * 7; /* 1 week */
     priv->override_macros = g_hash_table_new_full(g_str_hash, g_str_equal,
                                                   g_free, g_free);
+
+    (void) hif_context_get_base_arch(context);
+}
+
+/**
+ * hif_context_globals_init:
+ * @error: a #GError or %NULL
+ *
+ * Sadly RPM has process global data.  You should invoke
+ * this function early on in process startup.  If not,
+ * it will be invoked for you.
+ *
+ * Returns: %TRUE for success, %FALSE otherwise
+ *
+ * Since: 0.7.0
+ */
+gboolean
+hif_context_globals_init (GError **error)
+{
+    static gsize initialized = 0;
+    gboolean ret = TRUE;
+
+    if (g_once_init_enter (&initialized)) {
+
+        /* librepo's globals */
+        lr_global_init();
+
+        /* librpm's globals */
+        if (rpmReadConfigFiles(NULL, NULL) != 0) {
+            g_set_error_literal(error,
+                                HIF_ERROR,
+                                HIF_ERROR_INTERNAL_ERROR,
+                                "failed to read rpm config files");
+            ret = FALSE;
+        }
+
+        g_once_init_leave (&initialized, 1);
+    }
+    return ret;
 }
 
 /**
@@ -1370,13 +1409,8 @@ hif_context_setup(HifContext *context,
     if (cancellable != NULL)
         hif_state_set_cancellable(priv->state, cancellable);
 
-    if (rpmReadConfigFiles(NULL, NULL) != 0) {
-        g_set_error_literal(error,
-                            HIF_ERROR,
-                            HIF_ERROR_INTERNAL_ERROR,
-                            "failed to read rpm config files");
+    if (!hif_context_globals_init (error))
         return FALSE;
-    }
 
     buf = g_string_new("");
     g_hash_table_iter_init(&hashiter, priv->override_macros);
@@ -1870,6 +1904,5 @@ hif_context_new(void)
 {
     HifContext *context;
     context = g_object_new(HIF_TYPE_CONTEXT, NULL);
-    lr_global_init();
     return HIF_CONTEXT(context);
 }
