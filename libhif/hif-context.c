1df05f3e0c3a2629ec80389f2710d943b6d1187a
Jonathan Lebon <jlebon@redhat.com>
2016-05-12 10:20:25 -0400

    hif_context_install: revert back to all arches
    This is mostly a revert of commit 2f68fea (#50) with two modifications:
      1. use hy_query_filter_latest() instead of hy_query_latest_by_arch()
      2. just pick the first pkg if multiple packages have the same version
    
    The issue that #50 introduced is that multiple packages can be returned,
    not necessarily of the same NEVR. Since we just picked whatever the
    first package in the array was, we would sometimes be choosing older
    packages.
    
    Now, with hy_query_filter_latest(), we just always pick the latest NEVR,
    regardless of the arch (so it should still cover cases where a pkg goes
    from noarch to not noarch).
    
    Resolves: #120
    
    Closes: #121
    Approved by: ignatenkobrain

--- libhif/hif-context.c
+++ libhif/hif-context.c
@@ -44,7 +44,6 @@
 #include "hif-utils.h"
 #include "hif-sack.h"
 #include "hy-query.h"
-#include "hy-subject.h"
 
 #define MAX_NATIVE_ARCHES    12
 
@@ -1600,9 +1599,6 @@ hif_context_install (HifContext *context, const gchar *name, GError **error)
     g_autoptr(GPtrArray) pkglist = NULL;
     HifPackage *pkg;
     HyQuery query;
-    HySubject subject;
-    HyPossibilities poss;
-    HyNevra nevra;
     gboolean ret = TRUE;
 
     /* create sack and add sources */
@@ -1613,31 +1609,29 @@ hif_context_install (HifContext *context, const gchar *name, GError **error)
             return FALSE;
     }
 
-    subject = hy_subject_create(name);
-    poss = hy_subject_nevra_possibilities_real(subject, NULL, priv->sack, 0);
-    if (hy_possibilities_next_nevra(poss, &nevra) == 0) {
-        query = hy_nevra_to_query(nevra, priv->sack);
-        pkglist = hy_query_run(query);
-    } else {
-        g_set_error (error,
-                     HIF_ERROR,
-                     HIF_ERROR_PACKAGE_NOT_FOUND,
-                     "No package '%s' found", name);
-        return FALSE;
-    }
+    /* find a newest remote package to install */
+    query = hy_query_create(priv->sack);
+    hy_query_filter_latest(query, TRUE);
+    hy_query_filter_in(query, HY_PKG_ARCH, HY_EQ,
+                       (const gchar **) priv->native_arches);
+    hy_query_filter(query, HY_PKG_REPONAME, HY_NEQ, HY_SYSTEM_REPO_NAME);
+    hy_query_filter(query, HY_PKG_ARCH, HY_NEQ, "src");
+    hy_query_filter(query, HY_PKG_NAME, HY_EQ, name);
+    pkglist = hy_query_run(query);
 
-    if (pkglist->len > 0) {
-        pkg = pkglist->pdata[0];
-        hif_package_set_user_action(pkg, TRUE);
-        g_debug("adding %s-%s to goal", hif_package_get_name(pkg), hif_package_get_evr(pkg));
-        hy_goal_install(priv->goal, pkg);
-    } else {
+    if (pkglist->len == 0) {
         g_set_error(error,
                     HIF_ERROR,
                     HIF_ERROR_PACKAGE_NOT_FOUND,
-                    "Unable to find matching package for target '%s'", name);
+                    "No package '%s' found", name);
         return FALSE;
     }
+
+    /* add first package */
+    pkg = g_ptr_array_index (pkglist, 0);
+    hif_package_set_user_action(pkg, TRUE);
+    g_debug("adding %s-%s to goal", hif_package_get_name(pkg), hif_package_get_evr(pkg));
+    hy_goal_install(priv->goal, pkg);
 
     hy_query_free(query);
     return TRUE;
