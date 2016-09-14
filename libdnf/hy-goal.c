50ba2c7e1d973dad4ac483177d70c51a07e74be2
Jan Silhan <jsilhan@redhat.com>
2016-09-14 10:27:13 +0200

    fixup! selector: prefer obsoletes (Related:RhBug:1096506)

--- libdnf/hy-goal.c
+++ libdnf/hy-goal.c
@@ -524,23 +524,23 @@ filter_name2job(DnfSack *sack, const struct _Filter *f, Queue *job)
  * provide will be picked first
  */
 static void
-add_preferred_provide(HifSack *sack, Queue *job, Id id)
+add_preferred_provide(DnfSack *sack, Queue *job, Id id)
 {
     g_autoptr(GPtrArray) plist = g_ptr_array_new_with_free_func(
         (GDestroyNotify) g_object_unref);
-    Pool *pool = hif_sack_get_pool(sack);
+    Pool *pool = dnf_sack_get_pool(sack);
     const char *name = pool_dep2str(pool, id);
     HyQuery q = hy_query_create(sack);
     hy_query_filter(q, HY_PKG_NAME, HY_NEQ, name);
-    HifPackageSet *pset = hy_query_run_set(q);
+    DnfPackageSet *pset = hy_query_run_set(q);
     hy_query_filter(q, HY_PKG_PROVIDES, HY_EQ, name);
     hy_query_filter_package_in(q, HY_PKG_OBSOLETES, HY_NEQ, pset);
-    HifPackage *pkg;
+    DnfPackage *pkg;
     plist = hy_query_run(q);
     for (guint i = 0; i < plist->len; i++) {
         pkg = g_ptr_array_index(plist, i);
         queue_push2(job, SOLVER_DISFAVOR|SOLVER_SOLVABLE,
-                    hif_package_get_id(pkg));
+                    dnf_package_get_id(pkg));
     }
     queue_push2(job, SOLVER_SOLVABLE_PROVIDES, id);
     hy_query_free(q);
--- tests/hawkey/test_goal.c
+++ tests/hawkey/test_goal.c
@@ -243,7 +243,7 @@ START_TEST(test_goal_install_selector_obsoletes_first)
     assert_iueo(goal, 1, 0, 0, 0);
 
     GPtrArray *plist = hy_goal_list_installs(goal, NULL);
-    char *nvra = hif_package_get_nevra(g_ptr_array_index(plist, 0));
+    char *nvra = dnf_package_get_nevra(g_ptr_array_index(plist, 0));
     ck_assert_str_eq(nvra, "B-1-0.noarch");
     g_free(nvra);
     g_ptr_array_unref(plist);
