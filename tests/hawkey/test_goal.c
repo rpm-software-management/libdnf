cf3455098075a4c099a48ff19b8f1aba24847b83
Michael Mraka <michael.mraka@redhat.com>
2016-09-29 14:40:19 +0200

    hy_goal_describe_problem_rules() returns const char**
    /builddir/build/BUILD/libdnf-0.7.0-0.1277gfb39d85/python/hawkey/goal-py.c: In function 'describe_problem_rules':
    /builddir/build/BUILD/libdnf-0.7.0-0.1277gfb39d85/python/hawkey/goal-py.c:475:26: warning: initialization from incompatible pointer type [-Wincompatible-pointer-types]
         const char **plist = hy_goal_describe_problem_rules(self->goal,
                              ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    Closes: #196
    Approved by: cgwalters

--- tests/hawkey/test_goal.c
+++ tests/hawkey/test_goal.c
@@ -616,7 +616,7 @@ START_TEST(test_goal_describe_problem_rules)
     fail_unless(error->code == DNF_ERROR_NO_SOLUTION);
     fail_unless(hy_goal_count_problems(goal) > 0);
 
-    char **problems = hy_goal_describe_problem_rules(goal, 0);
+    const char **problems = hy_goal_describe_problem_rules(goal, 0);
     const char *expected[] = {
                 "conflicting requests",
                 "nothing provides goodbye needed by hello-1-1.noarch"
