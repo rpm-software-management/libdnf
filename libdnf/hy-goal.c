6cc877f39b6d52b4961985fe58c0da3ef1c13460
Kalev Lember <klember@redhat.com>
2016-12-17 11:56:42 +0100

    goal: Fix memory management for hy_goal_describe_problem_rules()
    As the function documentation states, the caller is responsible for
    freeing the returned value, which makes it inappropriate to mark this
    function as const char **. This commit removes the const and also fixes
    up the callers to properly free the returned array elements.

--- libdnf/hy-goal.c
+++ libdnf/hy-goal.c
@@ -1009,10 +1009,10 @@ hy_goal_describe_problem(HyGoal goal, unsigned i)
  *
  * Caller is responsible for freeing the returned string list.
  */
-const char **
+char **
 hy_goal_describe_problem_rules(HyGoal goal, unsigned i)
 {
-    const char **problist = NULL;
+    char **problist = NULL;
     int p = 0;
     /* internal error */
     if (i >= (unsigned) hy_goal_count_problems(goal))
