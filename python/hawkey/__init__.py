5900c963b6ce72588bb334c8fbca05df852f7f98
Jaroslav Mracek <jmracek@redhat.com>
2017-07-28 23:25:06 +0200

    Added deprecation warning for nevra_possibilities_real
    The function do not provide what is supposed. It is better to use
    'nevra_possibilities_real' and test each nevra in query.

--- python/hawkey/__init__.py
+++ python/hawkey/__init__.py
@@ -20,6 +20,7 @@
 
 from __future__ import absolute_import
 from sys import version_info as python_version
+import warnings
 
 from . import _hawkey
 import collections
@@ -392,6 +393,11 @@ class Subject(_hawkey.Subject):
             yield NEVRA(nevra=nevra)
 
     def nevra_possibilities_real(self, *args, **kwargs):
+        warnings.simplefilter('always', DeprecationWarning)
+        msg = "The function 'nevra_possibilities_real' is deprecated. " \
+              "Please use 'nevra_possibilities' instead."
+        warnings.warn(msg, DeprecationWarning)
+
         poss = super(Subject, self).nevra_possibilities_real(*args, **kwargs)
         for nevra in poss:
             yield NEVRA(nevra=nevra)
