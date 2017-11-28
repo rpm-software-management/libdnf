f2e88fd03b25ab3f31b605ea8fdb4484deba555e
Jaroslav Mracek <jmracek@redhat.com>
2017-11-28 19:24:08 +0100

    Remove unused function hy_goal_run_all()
    Closes: #380
    Approved by: m-blaha

--- libdnf/hy-goal.h
+++ libdnf/hy-goal.h
@@ -105,7 +105,6 @@ int hy_goal_req_length(HyGoal goal);
 /* resolving the goal */
 int hy_goal_run(HyGoal goal);
 int hy_goal_run_flags(HyGoal goal, DnfGoalActions flags);
-int hy_goal_run_all(HyGoal goal, hy_solution_callback cb, void *cb_data);
 int hy_goal_run_all_flags(HyGoal goal, hy_solution_callback cb, void *cb_data,
                           DnfGoalActions flags);
 
