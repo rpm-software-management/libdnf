0e015d603165cbddf079b541a4dcbc0277285955
Jaroslav Mracek <jmracek@redhat.com>
2017-12-04 17:23:30 +0100

    Unify hy_goal_downgrade...() behavior
    Now it should provide the same behavior for package and selector for
    hy_goal_downgrade... .

--- libdnf/hy-goal.h
+++ libdnf/hy-goal.h
@@ -73,8 +73,37 @@ DnfSack *hy_goal_get_sack(HyGoal goal);
 int hy_goal_distupgrade_all(HyGoal goal);
 int hy_goal_distupgrade(HyGoal goal, DnfPackage *new_pkg);
 int hy_goal_distupgrade_selector(HyGoal goal, HySelector);
+
+/**
+* @brief Mark a package to install. It doesn't check if the package has a lower version than
+* installed package or even installed. It allows to downgrade dependencies if needed. It return
+* always 0.
+*
+* @param goal HyGoal
+* @param new_pkg DnfPackage
+* @return int
+*/
 int hy_goal_downgrade_to(HyGoal goal, DnfPackage *new_pkg);
+
+/**
+* @brief Mark a content of selector to install. It doesn't check if packages have a lower version
+* than installed or even installed. It allows to downgrade dependencies if needed.
+*
+* @param goal HyGoal
+* @param sltr DnfPackage
+* @return int
+*/
 int hy_goal_downgrade_to_selector(HyGoal goal, HySelector sltr);
+
+/**
+* @brief Mark a content of selector to install. It doesn't check if packages have a lower version
+* than installed or even installed. In case that any package in selector can be install, it can be
+* skipped without an error. It allows to downgrade dependencies if needed.
+*
+* @param goal HyGoal
+* @param sltr DnfPackage
+* @return int
+*/
 int hy_goal_downgrade_to_selector_optional(HyGoal goal, HySelector sltr);
 int hy_goal_erase(HyGoal goal, DnfPackage *pkg);
 int hy_goal_erase_flags(HyGoal goal, DnfPackage *pkg, int flags);
