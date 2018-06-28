d42f6d53e057f004a06418e38daa9d699d418f85
Jaroslav Rohel <jrohel@redhat.com>
2018-06-28 12:49:51 +0200

    Implement class Finalizer
    Instance of this class calls user defined function during its destruction.

--- libdnf/utils/utils.hpp
+++ libdnf/utils/utils.hpp
@@ -1,8 +1,28 @@
 #ifndef LIBDNF_UTILS_HPP
 #define LIBDNF_UTILS_HPP
 
+#include <functional>
 #include <string>
 #include <vector>
+
+/**
+* @brief Object calls user defined function during its destruction.
+*
+* User function is passed as parameter of object constructor.
+* Example with user function defined as lambda:
+*
+*    Finalizer finalizerObject([&tmpDirectory](){
+*        remove(tmpDirectory);
+*    });
+*/
+class Finalizer {
+public:
+    Finalizer(const std::function<void()> & func) : func(func) {}
+    Finalizer(std::function<void()> && func) : func(std::move(func)) {}
+    ~Finalizer() { func(); }
+private:
+    std::function<void()> func;
+};
 
 namespace string {
 std::vector<std::string> split(const std::string &source, const char *delimiter, int maxSplit = -1);
