0145979fe580e66c25b6ae506801bf88ba76d766
Michal Domonkos <mdomonko@redhat.com>
2019-02-15 19:18:28 +0100

    [utils] Add getBaseArch()

--- libdnf/utils/os-release.cpp
+++ libdnf/utils/os-release.cpp
@@ -21,7 +21,37 @@
 #include "os-release.hpp"
 #include "File.hpp"
 
+#include <algorithm>
 #include <iostream>
+#include <rpm/rpmlib.h>
+#include <string>
+#include <vector>
+
+// basearch->{arch, ...} mapping, taken from DNF (dnf/rpm/__init__.py)
+std::map<std::string, std::vector<std::string>>
+archMap = {
+    { "aarch64",  { "aarch64" } },
+    { "alpha",    { "alpha", "alphaev4", "alphaev45", "alphaev5", "alphaev56", "alphaev6",
+                    "alphaev67", "alphaev68", "alphaev7", "alphapca56" } },
+    { "arm",      { "armv5tejl", "armv5tel", "armv5tl", "armv6l", "armv7l", "armv8l" } },
+    { "armhfp",   { "armv7hl", "armv7hnl", "armv8hl", "armv8hnl", "armv8hcnl" } },
+    { "i386",     { "i386", "athlon", "geode", "i386", "i486", "i586", "i686"} },
+    { "ia64",     { "ia64" } },
+    { "mips",     { "mips" } },
+    { "mipsel",   { "mipsel" } },
+    { "mips64",   { "mips64" } },
+    { "mips64el", { "mips64el" } },
+    { "noarch",   { "noarch" } },
+    { "ppc",      { "ppc" } },
+    { "ppc64",    { "ppc64", "ppc64iseries", "ppc64p7", "ppc64pseries" } },
+    { "ppc64le",  { "ppc64le" } },
+    { "s390",     { "s390" } },
+    { "s390x",    { "s390x" } },
+    { "sh3",      { "sh3" } },
+    { "sh4",      { "sh4", "sh4a" } },
+    { "sparc",    { "sparc", "sparc64", "sparc64v", "sparcv8", "sparcv9", "sparcv9v" } },
+    { "x86_64",   { "x86_64", "amd64", "ia32e" } },
+};
 
 std::map<std::string, std::string>
 getOsReleaseData(const std::string & path)
@@ -62,4 +92,37 @@ getOsReleaseData(const std::string & path)
         result.insert({key, value});
     }
     return result;
+}
+
+static void initLibRpm()
+{
+    static bool libRpmInitiated{false};
+    if (!libRpmInitiated) {
+        if (rpmReadConfigFiles(NULL, NULL) != 0) {
+            throw std::runtime_error("failed to read rpm config files\n");
+        }
+        libRpmInitiated = true;
+    }
+}
+
+std::string
+getBaseArch()
+{
+    const char *value;
+
+    initLibRpm();
+    // hy_detect_arch(&value);  // this is how DNF does it
+    rpmGetArchInfo(&value, NULL);  // let's just poke RPM
+    std::string arch(value);
+
+    // find the base architecture
+    for (auto const& i : archMap) {
+        if (std::any_of(i.second.begin(), i.second.end(),
+                        [arch](std::string j){return j == arch;})) {
+            return i.first;
+        }
+    }
+
+    // no match found
+    return "";
 }
