013c99fb91c3110e6501036ca08a906bbadbf7f9
Michal Domonkos <mdomonko@redhat.com>
2019-02-15 19:29:14 +0100

    [utils] Add getUserAgent()
    Closes: #768
    Approved by: jrohel

--- libdnf/utils/os-release.hpp
+++ libdnf/utils/os-release.hpp
@@ -21,8 +21,12 @@
 #include <map>
 #include <string>
 #include <vector>
+#include "libdnf/dnf-version.h"
+
+#define USER_AGENT "libdnf/" LIBDNF_VERSION
 
 namespace libdnf {
+
 /**
  * @brief Parse the os-release file and return its contents in a map.
  *
@@ -31,4 +35,27 @@ namespace libdnf {
  */
 std::map<std::string, std::string>
 getOsReleaseData(const std::vector<std::string> & paths);
+
+/**
+ * @brief Construct and return the User-Agent string for libdnf to send in HTTP
+ * headers.
+ *
+ * The format returned is as follows:
+ *
+ *   libdnf/VERSION (NAME VERSION_ID; VARIANT_ID; OS[.BASEARCH])
+ *
+ * where VERSION is the libdnf version, NAME, VERSION_ID and VARIANT_ID are OS
+ * identifiers read from the passed os-release data and OS and BASEARCH (if
+ * found) are the canonical OS name and base architecture, respectively,
+ * detected using RPM.
+ *
+ * Note that the OS part (enclosed in parentheses) will only be included for
+ * whitelisted values.
+ *
+ * @param  osReleaseData a map containing os-release data
+ * @return               a User-Agent string
+ */
+std::string
+getUserAgent(const std::map<std::string, std::string> & osReleaseData);
+
 }
