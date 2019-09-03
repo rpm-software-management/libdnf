c1cbffbaa24562d8c0e13319ef9c94d8e426c27a
Jaroslav Rohel <jrohel@redhat.com>
2019-09-03 12:55:51 +0200

    Added dnf_move_recursive()
    Moves a directory and its contents. If native move operations are
    supported then this is used, otherwise a copy + delete fallback is used.

--- libdnf/hy-iutil-private.hpp
+++ libdnf/hy-iutil-private.hpp
@@ -41,6 +41,7 @@ char *abspath(const char *path);
 int is_readable_rpm(const char *fn);
 int mkcachedir(char *path);
 gboolean mv(const char *old_path, const char *new_path, GError **error);
+gboolean dnf_move_recursive(const gchar *src_dir, const gchar *dst_dir, GError **error);
 char *this_username(void);
 
 /* misc utils */
