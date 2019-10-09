fafa6974ee0085326e8ef467b8ab9acaac7f5aa0
Jaroslav Rohel <jrohel@redhat.com>
2019-10-09 14:07:59 +0200

    libdnf::CompressedFile::open: Throw exception if file cannot be opened
    The "open" method silently ignored errors. The user got an exception
    "File 'xxx' is not opened" later and he did not know why.
    Now exception is thrown when error occurs - during open.
    In addition, the exception contains a detail of the error (No such file
    or directory, Permission denied, ...).
    
    Closes: #810
    Approved by: j-mracek

--- libdnf/utils/CompressedFile.cpp
+++ libdnf/utils/CompressedFile.cpp
@@ -6,6 +6,9 @@ extern "C" {
 #   include <solv/solv_xfopen.h>
 };
 
+#include <cerrno>
+#include <system_error>
+
 namespace libdnf {
 
 CompressedFile::CompressedFile(const std::string &filePath)
@@ -15,7 +18,16 @@ CompressedFile::~CompressedFile() = default;
 
 void CompressedFile::open(const char *mode)
 {
+    // There are situations when solv_xfopen returns NULL but it does not set errno.
+    // In this case errno value is random, so we set a default value.
+    errno = 0;
     file = solv_xfopen(filePath.c_str(), mode);
+    if (!file) {
+        if (errno) {
+            throw OpenException(filePath, std::system_category().message(errno));
+        }
+        throw OpenException(filePath);
+    }
 }
 
 std::string CompressedFile::getContent()
