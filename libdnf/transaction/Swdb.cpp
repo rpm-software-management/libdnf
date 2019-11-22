8d561e5c7bad91c3373f25218e9c6944203cc683
Jaroslav Mracek <jmracek@redhat.com>
2019-11-22 16:43:11 +0100

    Revert "Handle situation when an unprivileged user cannot create history database"
    It destroy DNF builds.
    
    This reverts commit 2659832b7d1ceedd45740a90d08c9f65800cd53a.

--- libdnf/transaction/Swdb.cpp
+++ libdnf/transaction/Swdb.cpp
@@ -59,22 +59,15 @@ Swdb::Swdb(const std::string &path)
         conn = std::make_shared< SQLite3 >(path);
         Transformer::createDatabase(conn);
     } else if (!pathExists(path.c_str())) {
-        if (geteuid() == 0) {
-            // writing to a file that doesn't exist and must be created
+        // writing to a file that doesn't exist and must be created
 
-            // extract persistdir from path - "/var/lib/dnf/"
-            auto found = path.find_last_of("/");
+        // extract persistdir from path - "/var/lib/dnf/"
+        auto found = path.find_last_of("/");
 
-            Transformer transformer(path.substr(0, found), path);
-            transformer.transform();
+        Transformer transformer(path.substr(0, found), path);
+        transformer.transform();
 
-            conn = std::make_shared< SQLite3 >(path);
-        } else {
-            // History db doesn't exist and an unprivileged user can't create it.
-            // As a fail-safe, open a connection to an empty in-memory database.
-            conn = std::make_shared< SQLite3 >(":memory:");
-            Transformer::createDatabase(conn);
-        }
+        conn = std::make_shared< SQLite3 >(path);
     } else {
         // writing to an existing file
         conn = std::make_shared< SQLite3 >(path);
