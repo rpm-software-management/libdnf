3751f423f8744292acfe0ac0553b7e88f5fa3b66
Daniel Mach <dmach@redhat.com>
2020-01-07 08:38:01 +0100

    Fix indentation.

--- libdnf/utils/number.hpp
+++ libdnf/utils/number.hpp
@@ -2,6 +2,8 @@
 
 
 namespace libdnf::utils::number {
-    /// @replaces libdnf:utils.hpp:function:random(const int min, const int max)
-    int random_int(int min, int max);
-}
+
+/// @replaces libdnf:utils.hpp:function:random(const int min, const int max)
+int random_int(int min, int max);
+
+}  // namespace libdnf::utils::number
--- libdnf/utils/string.hpp
+++ libdnf/utils/string.hpp
@@ -3,39 +3,40 @@
 
 
 namespace libdnf::utils::string {
-    /// @replaces libdnf:utils.hpp:function:fromCstring(const char * cstring);
-    std::string from_cstring(const char * cstring);
 
-    // SPLIT
+/// @replaces libdnf:utils.hpp:function:fromCstring(const char * cstring);
+std::string from_cstring(const char * cstring);
 
-    /// @replaces libdnf:utils.hpp:function:split(const std::string & source, const char * delimiter, int maxSplit)
-    std::vector<std::string> split(const std::string & str, const char * separator, int maxsplit);
+// SPLIT
 
-    /// @replaces libdnf:utils.hpp:function:rsplit(const std::string & source, const char * delimiter, int maxSplit)
-    std::vector<std::string> rsplit(const std::string & str, const char * separator, int maxsplit);
+/// @replaces libdnf:utils.hpp:function:split(const std::string & source, const char * delimiter, int maxSplit)
+std::vector<std::string> split(const std::string & str, const char * separator, int maxsplit);
 
-    // STARTS/ENDS WITH
+/// @replaces libdnf:utils.hpp:function:rsplit(const std::string & source, const char * delimiter, int maxSplit)
+std::vector<std::string> rsplit(const std::string & str, const char * separator, int maxsplit);
 
-    /// @replaces libdnf:utils.hpp:function:startsWith(const std::string & source, const std::string & toMatch)
-    bool startswith(const std::string & str, const std::string & prefix);
+// STARTS/ENDS WITH
 
-    /// @replaces libdnf:utils.hpp:function:endsWith(const std::string & source, const std::string & toMatch)
-    bool endswith(const std::string & str, const std::string & suffix);
+/// @replaces libdnf:utils.hpp:function:startsWith(const std::string & source, const std::string & toMatch)
+bool startswith(const std::string & str, const std::string & prefix);
 
-    // STRIP
+/// @replaces libdnf:utils.hpp:function:endsWith(const std::string & source, const std::string & toMatch)
+bool endswith(const std::string & str, const std::string & suffix);
 
-    /// @replaces libdnf:utils.hpp:function:trim(const std::string & source)
-    std::string strip(const std::string & str);
-    std::string strip(const std::string & str, const std::string & chars);
+// STRIP
 
-    std::string lstrip(const std::string & str);
-    std::string lstrip(const std::string & str, const std::string & chars);
+/// @replaces libdnf:utils.hpp:function:trim(const std::string & source)
+std::string strip(const std::string & str);
+std::string strip(const std::string & str, const std::string & chars);
 
-    std::string rstrip(const std::string & str);
-    std::string rstrip(const std::string & str, const std::string & chars);
+std::string lstrip(const std::string & str);
+std::string lstrip(const std::string & str, const std::string & chars);
 
-    // PREFIX/SUFFIX REMOVAL
+std::string rstrip(const std::string & str);
+std::string rstrip(const std::string & str, const std::string & chars);
 
-    std::string remove_suffix(const std::string & str, const std::string & suffix);
-    std::string remove_prefix(const std::string & str, const std::string & prefix);
-}
+// PREFIX/SUFFIX REMOVAL
+std::string remove_suffix(const std::string & str, const std::string & suffix);
+std::string remove_prefix(const std::string & str, const std::string & prefix);
+
+}  // namespace libdnf::utils::string
