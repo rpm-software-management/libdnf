c58f90f5632c927a8ca251404864fd557d7b4fe6
Aleš Matěj <amatej@redhat.com>
2020-01-30 16:28:50 +0100

    Update ModuleDependencies to libmodulemd-2 API

--- libdnf/module/modulemd/ModuleDependencies.cpp
+++ libdnf/module/modulemd/ModuleDependencies.cpp
@@ -29,46 +29,31 @@ ModuleDependencies::ModuleDependencies(ModulemdDependencies *dependencies)
         : dependencies(dependencies)
 {}
 
-std::vector <std::map<std::string, std::vector<std::string>>> ModuleDependencies::getRequires() const
+std::vector<std::map<std::string, std::vector<std::string>>> ModuleDependencies::getRequires() const
 {
     if (!dependencies) {
         return {};
     }
-    auto cRequires = modulemd_dependencies_peek_requires(dependencies);
-    return getRequirements(cRequires);
-}
+    std::vector<std::map<std::string, std::vector<std::string>>> resultRequires;
+    // In order to preserve API we return vector of maps but there can ever only be one map
+    resultRequires.reserve(1);
+    std::map<std::string, std::vector<std::string>> moduleRequirements;
 
-std::map<std::string, std::vector<std::string>> ModuleDependencies::wrapModuleDependencies(const char *moduleName, ModulemdSimpleSet *streams) const
-{
-    std::map<std::string, std::vector<std::string> > moduleRequirements;
-    auto streamSet = modulemd_simpleset_dup(streams);
-    auto & moduleStreamVector = moduleRequirements[moduleName];
-    for (auto item = streamSet; *item; ++item) {
-        moduleStreamVector.emplace_back(*item);
-        g_free(*item);
+    char** runtimeReqModules = modulemd_dependencies_get_runtime_modules_as_strv(dependencies);
+
+    for (char **iterModule = runtimeReqModules; iterModule && *iterModule; iterModule++) {
+        char** runtimeReqStreams = modulemd_dependencies_get_runtime_streams_as_strv(dependencies, *iterModule);
+        std::string moduleName = static_cast<char *>(*iterModule);
+        auto & moduleStreamVector = moduleRequirements[moduleName];
+        for (char **iterStream = runtimeReqStreams; iterStream && *iterStream; iterStream++) {
+            moduleStreamVector.emplace_back(*iterStream);
+        }
+        g_strfreev(runtimeReqStreams);
     }
-    g_free(streamSet);
+    resultRequires.push_back(moduleRequirements);
+    g_strfreev(runtimeReqModules);
 
-    return moduleRequirements;
-}
-
-std::vector<std::map<std::string, std::vector<std::string>>> ModuleDependencies::getRequirements(GHashTable *requirements) const
-{
-    std::vector<std::map<std::string, std::vector<std::string>>> requires;
-    requires.reserve(g_hash_table_size(requirements));
-
-    GHashTableIter iterator;
-    gpointer key, value;
-
-    g_hash_table_iter_init (&iterator, requirements);
-    while (g_hash_table_iter_next(&iterator, &key, &value)) {
-        auto moduleRequirements = wrapModuleDependencies(static_cast<const char *>(key),
-                                                         static_cast<ModulemdSimpleSet *>(value));
-
-        requires.push_back(moduleRequirements);
-    }
-
-    return requires;
+    return resultRequires;
 }
 
 }
