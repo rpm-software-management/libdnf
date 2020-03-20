1f58b9b6a56643ddbf399080286800aa9c228d5e
Jaroslav Rohel <jrohel@redhat.com>
2020-03-20 09:30:06 +0100

    [dnf5] Modify additional config option types for the new libdnf
    OptionEnum, OptionStringList

--- include/libdnf/conf/option_enum.hpp
+++ include/libdnf/conf/option_enum.hpp
@@ -1,29 +1,26 @@
 /*
- * Copyright (C) 2018 Red Hat, Inc.
- *
- * Licensed under the GNU Lesser General Public License Version 2.1
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
- */
+Copyright (C) 2020 Red Hat, Inc.
 
-#ifndef _LIBDNF_OPTION_ENUM_HPP
-#define _LIBDNF_OPTION_ENUM_HPP
+This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
 
-#ifdef LIBDNF_UNSTABLE_API
+Libdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
 
-#include "Option.hpp"
+Libdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef LIBDNF_CONF_OPTION_ENUM_HPP
+#define LIBDNF_CONF_OPTION_ENUM_HPP
+
+#include "option.hpp"
 
 #include <functional>
 #include <vector>
@@ -33,83 +30,98 @@ namespace libdnf {
 template <typename T>
 class OptionEnum : public Option {
 public:
-    typedef T ValueType;
-    typedef std::function<ValueType (const std::string &)> FromStringFunc;
+    using ValueType = T;
+    using FromStringFunc = std::function<ValueType(const std::string &)>;
 
-    OptionEnum(ValueType defaultValue, const std::vector<ValueType> & enumVals);
-    OptionEnum(ValueType defaultValue, std::vector<ValueType> && enumVals);
-    OptionEnum(ValueType defaultValue, const std::vector<ValueType> & enumVals, FromStringFunc && fromStringFunc);
-    OptionEnum(ValueType defaultValue, std::vector<ValueType> && enumVals, FromStringFunc && fromStringFunc);
+    class InvalidValue : public Option::InvalidValue {
+    public:
+        using Option::InvalidValue::InvalidValue;
+        const char * get_domain_name() const noexcept override { return "libdnf::OptionEnum"; }
+    };
+
+    class NotAllowedValue : public InvalidValue {
+    public:
+        using InvalidValue::InvalidValue;
+        const char * get_name() const noexcept override { return "NotAllowedValue"; }
+    };
+
+    OptionEnum(ValueType default_value, const std::vector<ValueType> & enum_vals);
+    OptionEnum(ValueType default_value, std::vector<ValueType> && enum_vals);
+    OptionEnum(ValueType default_value, const std::vector<ValueType> & enum_vals, FromStringFunc && from_string_func);
+    OptionEnum(ValueType default_value, std::vector<ValueType> && enum_vals, FromStringFunc && from_string_func);
     OptionEnum * clone() const override;
     void test(ValueType value) const;
-    ValueType fromString(const std::string & value) const;
+    ValueType from_string(const std::string & value) const;
     void set(Priority priority, ValueType value);
     void set(Priority priority, const std::string & value) override;
-    T getValue() const;
-    T getDefaultValue() const;
-    std::string toString(ValueType value) const;
-    std::string getValueString() const override;
+    T get_value() const;
+    T get_default_value() const;
+    std::string to_string(ValueType value) const;
+    std::string get_value_string() const override;
 
-protected:
-    FromStringFunc fromStringUser;
-    std::vector<ValueType> enumVals;
-    ValueType defaultValue;
+private:
+    FromStringFunc from_string_user;
+    std::vector<ValueType> enum_vals;
+    ValueType default_value;
     ValueType value;
 };
 
 template <>
 class OptionEnum<std::string> : public Option {
 public:
-    typedef std::string ValueType;
-    typedef std::function<ValueType (const std::string &)> FromStringFunc;
+    using ValueType = std::string;
+    using FromStringFunc = std::function<ValueType(const std::string &)>;
 
-    OptionEnum(const std::string & defaultValue, const std::vector<ValueType> & enumVals);
-    OptionEnum(const std::string & defaultValue, std::vector<ValueType> && enumVals);
-    OptionEnum(const std::string & defaultValue, const std::vector<ValueType> & enumVals, FromStringFunc && fromStringFunc);
-    OptionEnum(const std::string & defaultValue, std::vector<ValueType> && enumVals, FromStringFunc && fromStringFunc);
+    class InvalidValue : public Option::InvalidValue {
+    public:
+        using Option::InvalidValue::InvalidValue;
+        const char * get_domain_name() const noexcept override { return "libdnf::OptionEnum"; }
+    };
+
+    class NotAllowedValue : public InvalidValue {
+    public:
+        using InvalidValue::InvalidValue;
+        const char * get_name() const noexcept override { return "NotAllowedValue"; }
+    };
+
+    OptionEnum(const std::string & default_value, std::vector<ValueType> enum_vals);
+    OptionEnum(const std::string & default_value, std::vector<ValueType> enum_vals, FromStringFunc && from_string_func);
     OptionEnum * clone() const override;
     void test(const std::string & value) const;
-    std::string fromString(const std::string & value) const;
+    std::string from_string(const std::string & value) const;
     void set(Priority priority, const std::string & value) override;
-    const std::string & getValue() const;
-    const std::string & getDefaultValue() const;
-    std::string getValueString() const override;
+    const std::string & get_value() const;
+    const std::string & get_default_value() const;
+    std::string get_value_string() const override;
 
-protected:
-    FromStringFunc fromStringUser;
-    std::vector<ValueType> enumVals;
-    ValueType defaultValue;
+private:
+    FromStringFunc from_string_user;
+    std::vector<ValueType> enum_vals;
+    ValueType default_value;
     ValueType value;
 };
 
 template <typename T>
-inline OptionEnum<T> * OptionEnum<T>::clone() const
-{
+inline OptionEnum<T> * OptionEnum<T>::clone() const {
     return new OptionEnum<T>(*this);
 }
 
-inline OptionEnum<std::string> * OptionEnum<std::string>::clone() const
-{
+inline OptionEnum<std::string> * OptionEnum<std::string>::clone() const {
     return new OptionEnum<std::string>(*this);
 }
 
-inline const std::string & OptionEnum<std::string>::getValue() const
-{
+inline const std::string & OptionEnum<std::string>::get_value() const {
     return value;
 }
 
-inline const std::string & OptionEnum<std::string>::getDefaultValue() const
-{
-    return defaultValue;
+inline const std::string & OptionEnum<std::string>::get_default_value() const {
+    return default_value;
 }
 
-inline std::string OptionEnum<std::string>::getValueString() const
-{
+inline std::string OptionEnum<std::string>::get_value_string() const {
     return value;
 }
 
-}
-
-#endif
+}  // namespace libdnf
 
 #endif
--- include/libdnf/conf/option_string_list.hpp
+++ include/libdnf/conf/option_string_list.hpp
@@ -1,80 +1,79 @@
 /*
- * Copyright (C) 2018 Red Hat, Inc.
- *
- * Licensed under the GNU Lesser General Public License Version 2.1
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
- */
+Copyright (C) 2020 Red Hat, Inc.
 
-#ifndef _LIBDNF_OPTION_STRING_LIST_HPP
-#define _LIBDNF_OPTION_STRING_LIST_HPP
+This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
 
-#ifdef LIBDNF_UNSTABLE_API
+Libdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
 
-#include "Option.hpp"
+Libdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef LIBDNF_CONF_OPTION_STRING_LIST_HPP
+#define LIBDNF_CONF_OPTION_STRING_LIST_HPP
+
+#include "option.hpp"
+
 #include <vector>
 
 namespace libdnf {
 
 class OptionStringList : public Option {
 public:
-    typedef std::vector<std::string> ValueType;
+    using ValueType = std::vector<std::string>;
 
-    OptionStringList(const ValueType & defaultValue);
-    OptionStringList(const ValueType & defaultValue, const std::string & regex, bool icase);
-    OptionStringList(const std::string & defaultValue);
-    OptionStringList(const std::string & defaultValue, const std::string & regex, bool icase);
+    class NotAllowedValue : public InvalidValue {
+    public:
+        using InvalidValue::InvalidValue;
+        const char * get_domain_name() const noexcept override { return "libdnf::OptionStringList"; }
+        const char * get_name() const noexcept override { return "NotAllowedValue"; }
+    };
+
+    OptionStringList(const ValueType & default_value);
+    OptionStringList(const ValueType & default_value, std::string regex, bool icase);
+    OptionStringList(const std::string & default_value);
+    OptionStringList(const std::string & default_value, std::string regex, bool icase);
     OptionStringList * clone() const override;
     void test(const std::vector<std::string> & value) const;
-    ValueType fromString(const std::string & value) const;
+    ValueType from_string(const std::string & value) const;
     virtual void set(Priority priority, const ValueType & value);
     void set(Priority priority, const std::string & value) override;
-    const ValueType & getValue() const;
-    const ValueType & getDefaultValue() const;
-    std::string toString(const ValueType & value) const;
-    std::string getValueString() const override;
+    const ValueType & get_value() const;
+    const ValueType & get_default_value() const;
+    std::string to_string(const ValueType & value) const;
+    std::string get_value_string() const override;
 
 protected:
     std::string regex;
     bool icase;
-    ValueType defaultValue;
+    ValueType default_value;
     ValueType value;
 };
 
-inline OptionStringList * OptionStringList::clone() const
-{
+inline OptionStringList * OptionStringList::clone() const {
     return new OptionStringList(*this);
 }
 
-inline const OptionStringList::ValueType & OptionStringList::getValue() const
-{
+inline const OptionStringList::ValueType & OptionStringList::get_value() const {
     return value;
 }
 
-inline const OptionStringList::ValueType & OptionStringList::getDefaultValue() const
-{
-    return defaultValue;
+inline const OptionStringList::ValueType & OptionStringList::get_default_value() const {
+    return default_value;
 }
 
-inline std::string OptionStringList::getValueString() const
-{
-    return toString(value); 
+inline std::string OptionStringList::get_value_string() const {
+    return to_string(value);
 }
 
-}
-
-#endif
+}  // namespace libdnf
 
 #endif
