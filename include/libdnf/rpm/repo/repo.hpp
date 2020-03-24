81298f27cc2bf7e4595528b601c66133bca78ce5
Jaroslav Rohel <jrohel@redhat.com>
2020-03-24 17:38:58 +0100

    [dnf5] Modify rpm repository support (rpm::Repo) for new libdnf

--- include/libdnf/rpm/repo/repo.hpp
+++ include/libdnf/rpm/repo/repo.hpp
@@ -1,41 +1,49 @@
 /*
- * Copyright (C) 2018 Red Hat, Inc.
- *
- * Licensed under the GNU Lesser General Public License Version 2.1
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
- */
+Copyright (C) 2020 Red Hat, Inc.
 
-#ifndef _LIBDNF_REPO_HPP
-#define _LIBDNF_REPO_HPP
+This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
+
+Libdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Libdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef LIBDNF_RPM_REPO_REPO_HPP
+#define LIBDNF_RPM_REPO_REPO_HPP
 
 #define MODULEMD
 
-#include "../conf/ConfigRepo.hpp"
-#include "../hy-types.h"
+#include "config_repo.hpp"
 
 #include <memory>
 #include <stdexcept>
 
 namespace libdnf {
 
-class LrException : public std::runtime_error {
+class Base;
+
+}  // namespace libdnf
+
+namespace libdnf::rpm {
+
+class LrException : public RuntimeError {
 public:
-    LrException(int code, const char * msg) : runtime_error(msg), code(code) {}
-    LrException(int code, const std::string & msg) : runtime_error(msg), code(code) {}
-    int getCode() const noexcept { return code; }
+    LrException(int code, const char * msg) : RuntimeError(msg), code(code) {}
+    LrException(int code, const std::string & msg) : RuntimeError(msg), code(code) {}
+    const char * get_domain_name() const noexcept override { return "librepo"; }
+    const char * get_name() const noexcept override { return "LrException"; }
+    const char * get_description() const noexcept override { return "Librepo exception"; }
+    int get_code() const noexcept { return code; }
+
 private:
     int code;
 };
@@ -80,13 +88,17 @@ public:
             (Do not modify or free the string) */
     };
 
-    virtual void start(const char *what) {}
+    virtual void start(const char * /*what*/) {}
     virtual void end() {}
-    virtual int progress(double totalToDownload, double downloaded);
-    virtual void fastestMirror(FastestMirrorStage stage, const char *msg);
-    virtual int handleMirrorFailure(const char *msg, const char *url, const char *metadata);
-    virtual bool repokeyImport(const std::string & id, const std::string & userId,
-                               const std::string & fingerprint, const std::string & url, long int timestamp);
+    virtual int progress(double total_to_download, double downloaded);
+    virtual void fastest_mirror(FastestMirrorStage stage, const char * msg);
+    virtual int handle_mirror_failure(const char * msg, const char * url, const char * metadata);
+    virtual bool repokey_import(
+        const std::string & id,
+        const std::string & user_id,
+        const std::string & fingerprint,
+        const std::string & url,
+        long int timestamp);
     virtual ~RepoCB() = default;
 };
 
@@ -99,14 +111,9 @@ public:
 * Remote metadata is cached locally.
 *
 */
-struct Repo {
+class Repo {
 public:
-
-    enum class Type {
-        AVAILABLE,
-        SYSTEM,
-        COMMANDLINE
-    };
+    enum class Type { AVAILABLE, SYSTEM, COMMANDLINE };
 
     enum class SyncStrategy {
         // use the local cache even if it's expired. download if there's no cache.
@@ -124,7 +131,7 @@ public:
     * @param id repo ID to verify
     * @return   index of the first invalid character in the repo ID (if present) or -1
     */
-    static int verifyId(const std::string & id);
+    static int verify_id(const std::string & id);
 
     /**
     * @brief Construct the Repo object
@@ -132,11 +139,15 @@ public:
     * @param id     repo ID to use
     * @param conf   configuration to use
     */
-    Repo(const std::string & id, std::unique_ptr<ConfigRepo> && conf, Repo::Type type = Repo::Type::AVAILABLE);
+    Repo(
+        const std::string & id,
+        std::unique_ptr<ConfigRepo> && conf,
+        Base & base,
+        Repo::Type type = Repo::Type::AVAILABLE);
 
-    Repo & operator =(Repo && repo) = delete;
+    Repo & operator=(Repo && repo) = delete;
 
-    void setCallbacks(std::unique_ptr<RepoCB> && callbacks);
+    void set_callbacks(std::unique_ptr<RepoCB> && callbacks);
 
     /**
     * @brief Verify repo object configuration
@@ -144,12 +155,12 @@ public:
     * Will throw exception if Repo has no mirror or baseurl set or if Repo type is unsupported.
     */
     void verify() const;
-    ConfigRepo * getConfig() noexcept;
-    const std::string & getId() const noexcept;
+    ConfigRepo * get_config() noexcept;
+    const std::string & get_id() const noexcept;
     void enable();
     void disable();
-    bool isEnabled() const;
-    bool isLocal() const;
+    bool is_enabled() const;
+    bool is_local() const;
     /**
     * @brief Initialize the repo with metadata
     *
@@ -158,20 +169,20 @@ public:
     * @return true if fresh metadata were downloaded, false otherwise.
     */
     bool load();
-    bool loadCache(bool throwExcept);
-    void downloadMetadata(const std::string & destdir);
-    bool getUseIncludes() const;
-    void setUseIncludes(bool enabled);
-    bool getLoadMetadataOther() const;
-    void setLoadMetadataOther(bool value);
-    int getCost() const;
-    int getPriority() const;
-    std::string getCompsFn();  // this is temporarily made public for DNF compatibility
+    bool load_cache(bool throw_except);
+    void download_metadata(const std::string & destdir);
+    bool get_use_includes() const;
+    void set_use_includes(bool enabled);
+    bool get_load_metadata_other() const;
+    void set_load_metadata_other(bool value);
+    int get_cost() const;
+    int get_priority() const;
+    std::string get_comps_fn();  // this is temporarily made public for DNF compatibility
 #ifdef MODULEMD
-    std::string getModulesFn(); // temporary made public
+    std::string get_modules_fn();  // temporary made public
 #endif
-    const std::string & getRevision() const;
-    int getAge() const;
+    const std::string & get_revision() const;
+    int64_t get_age() const;
 
     /**
     * @brief Ask for additional repository metadata type to download
@@ -180,7 +191,7 @@ public:
     *
     * @param metadataType metadata type (filelists, other, productid...)
     */
-    void addMetadataTypeToDownload(const std::string &metadataType);
+    void add_metadata_type_to_download(const std::string & metadata_type);
 
     /**
     * @brief Stop asking for this additional repository metadata type
@@ -190,7 +201,7 @@ public:
     *
     * @param metadataType metadata type (filelists, other, productid...)
     */
-    void removeMetadataTypeFromDownload(const std::string &metadataType);
+    void remove_metadata_type_from_download(const std::string & metadata_type);
 
     /**
     * @brief Return path to the particular downloaded repository metadata in cache
@@ -199,7 +210,7 @@ public:
     *
     * @return file path or empty string in case the requested metadata does not exist
     */
-    std::string getMetadataPath(const std::string &metadataType);
+    std::string get_metadata_path(const std::string & metadata_type);
 
     /**
     * @brief Return content of the particular downloaded repository metadata
@@ -210,7 +221,7 @@ public:
     *
     * @return content of metadata file or empty string in case the requested metadata does not exist
     */
-    std::string getMetadataContent(const std::string &metadataType);
+    //std::string get_metadata_content(const std::string & metadataType);
 
     /**
     * @brief Mark whatever is in the current cache expired.
@@ -225,7 +236,7 @@ public:
     *
     * @return bool
     */
-    bool isExpired() const;
+    bool is_expired() const;
 
     /**
     * @brief Get the number of seconds after which the cached metadata will expire.
@@ -234,11 +245,11 @@ public:
     *
     * @return Seconds to expiration
     */
-    int getExpiresIn() const;
+    int get_expires_in() const;
     bool fresh();
-    void setMaxMirrorTries(int maxMirrorTries);
-    int getTimestamp() const;
-    int getMaxTimestamp();
+    void set_max_mirror_tries(int max_mirror_tries);
+    int64_t get_timestamp() const;
+    int get_max_timestamp();
 
     /**
     * @brief Try to preserve remote side timestamps
@@ -250,11 +261,11 @@ public:
     *
     * @param preserveRemoteTime true - use remote file timestamp, false - use the current time
     */
-    void setPreserveRemoteTime(bool preserveRemoteTime);
-    bool getPreserveRemoteTime() const;
+    void set_preserve_remote_time(bool preserve_remote_time);
+    bool get_preserve_remote_time() const;
 
-    const std::vector<std::string> & getContentTags();
-    const std::vector<std::pair<std::string, std::string>> & getDistroTags();
+    const std::vector<std::string> & get_content_tags();
+    const std::vector<std::pair<std::string, std::string>> & get_distro_tags();
 
     /**
     * @brief Get list of relative locations of metadata files inside the repo
@@ -263,14 +274,14 @@ public:
     *
     * @return vector of (metadata_type, location) string pairs
     */
-    const std::vector<std::pair<std::string, std::string>> getMetadataLocations() const;
+    const std::vector<std::pair<std::string, std::string>> get_metadata_locations() const;
 
-    std::string getCachedir() const;
-    void setRepoFilePath(const std::string & path);
-    const std::string & getRepoFilePath() const noexcept;
-    void setSyncStrategy(SyncStrategy strategy);
-    SyncStrategy getSyncStrategy() const noexcept;
-    void downloadUrl(const char * url, int fd);
+    std::string get_cachedir() const;
+    void set_repo_file_path(const std::string & path);
+    const std::string & get_repo_file_path() const noexcept;
+    void set_sync_strategy(SyncStrategy strategy);
+    SyncStrategy get_sync_strategy() const noexcept;
+    void download_url(const char * url, int fd);
 
     /**
     * @brief Set http headers.
@@ -280,30 +291,30 @@ public:
     *
     * @param headers nullptr terminated array of C strings
     */
-    void setHttpHeaders(const char * headers[]);
+    void set_http_headers(const char * headers[]);
 
     /**
     * @brief Get array of added/changed/removed http headers.
     *
     * @return nullptr terminated array of C strings
     */
-    const char * const * getHttpHeaders() const;
-    std::vector<std::string> getMirrors() const;
+    const char * const * get_http_headers() const;
+    std::vector<std::string> get_mirrors() const;
 
-    void setSubstitutions(const std::map<std::string, std::string> & substitutions);
+    void set_substitutions(const std::map<std::string, std::string> & substitutions);
 
     ~Repo();
 
     class Impl;
+
 private:
     friend struct PackageTarget;
-    friend Impl * repoGetImpl(Repo * repo);
-    std::unique_ptr<Impl> pImpl;
+    std::unique_ptr<Impl> p_impl;
 };
 
 struct Downloader {
 public:
-    static void downloadURL(ConfigMain * cfg, const char * url, int fd);
+    static void download_url(ConfigMain * cfg, const char * url, int fd);
 };
 
 /**
@@ -316,15 +327,11 @@ public:
 class PackageTargetCB {
 public:
     /** Transfer status codes */
-    enum class TransferStatus {
-        SUCCESSFUL,
-        ALREADYEXISTS,
-        ERROR
-    };
+    enum class TransferStatus { SUCCESSFUL, ALREADYEXISTS, ERROR };
 
     virtual int end(TransferStatus status, const char * msg);
-    virtual int progress(double totalToDownload, double downloaded);
-    virtual int mirrorFailure(const char *msg, const char *url);
+    virtual int progress(double total_to_download, double downloaded);
+    virtual int mirror_failure(const char * msg, const char * url);
     virtual ~PackageTargetCB() = default;
 };
 
@@ -340,40 +347,59 @@ public:
     */
     enum class ChecksumType {
         UNKNOWN,
-        MD5,        /*    The most weakest hash */
-        SHA1,       /*  |                       */
-        SHA224,     /*  |                       */
-        SHA256,     /*  |                       */
-        SHA384,     /* \|/                      */
-        SHA512,     /*    The most secure hash  */
+        MD5,    /*    The most weakest hash */
+        SHA1,   /*  |                       */
+        SHA224, /*  |                       */
+        SHA256, /*  |                       */
+        SHA384, /* \|/                      */
+        SHA512, /*    The most secure hash  */
     };
 
-    static ChecksumType checksumType(const std::string & name);
-    static void downloadPackages(std::vector<PackageTarget *> & targets, bool failFast);
+    static ChecksumType checksum_type(const std::string & name);
+    static void download_packages(std::vector<PackageTarget *> & targets, bool fail_fast);
 
-    PackageTarget(Repo * repo, const char * relativeUrl, const char * dest, int chksType,
-                  const char * chksum, int64_t expectedSize, const char * baseUrl, bool resume,
-                  int64_t byteRangeStart, int64_t byteRangeEnd, PackageTargetCB * callbacks);
-    PackageTarget(ConfigMain * cfg, const char * relativeUrl, const char * dest, int chksType,
-                  const char * chksum, int64_t expectedSize, const char * baseUrl, bool resume,
-                  int64_t byteRangeStart, int64_t byteRangeEnd, PackageTargetCB * callbacks,
-                  const char * httpHeaders[] = nullptr);
+    PackageTarget(
+        Repo * repo,
+        const char * relative_url,
+        const char * dest,
+        int chks_type,
+        const char * chksum,
+        int64_t expected_size,
+        const char * base_url,
+        bool resume,
+        int64_t byte_range_start,
+        int64_t byte_range_end,
+        PackageTargetCB * callbacks);
+    PackageTarget(
+        ConfigMain * cfg,
+        const char * relative_url,
+        const char * dest,
+        int chks_type,
+        const char * chksum,
+        int64_t expected_size,
+        const char * base_url,
+        bool resume,
+        int64_t byte_range_start,
+        int64_t byte_range_end,
+        PackageTargetCB * callbacks,
+        const char * http_headers[] = nullptr);
     ~PackageTarget();
 
-    PackageTargetCB * getCallbacks();
-    const char * getErr();
+    PackageTargetCB * get_callbacks();
+    const char * get_err();
+
 private:
     class Impl;
-    std::unique_ptr<Impl> pImpl;
+    std::unique_ptr<Impl> p_impl;
 };
 
 struct LibrepoLog {
 public:
-    static long addHandler(const std::string & filePath, bool debug = false);
-    static void removeHandler(long uid);
-    static void removeAllHandlers();
+    static long add_handler(const std::string & file_path, bool debug = false);
+    static void remove_handler(long uid);
+    static void remove_all_handlers();
 };
 
-}
+}  // namespace libdnf::rpm
 
 #endif
