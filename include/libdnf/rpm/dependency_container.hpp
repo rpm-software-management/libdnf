3da50492d54fca860031991f840310f2fd611f74
Jaroslav Mracek <jmracek@redhat.com>
2020-04-09 09:02:37 +0200

    Move Dependency structures from dnf4 directory
    This is initial commit before refactoring

--- include/libdnf/rpm/dependency_container.hpp
+++ include/libdnf/rpm/dependency_container.hpp
@@ -1,22 +1,22 @@
 /*
- * Copyright (C) 2018 Red Hat, Inc.
- *
- * Licensed under the GNU Lesser General Public License Version 2.1
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
- */
+Copyright (C) 2018-2020 Red Hat, Inc.
+
+This file is part of libdnf: https://github.com/rpm-software-management/libdnf/
+
+Libdnf is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Libdnf is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with libdnf.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 
 #ifndef LIBDNF_DEPENDENCYCONTAINER_HPP
 #define LIBDNF_DEPENDENCYCONTAINER_HPP
@@ -28,9 +28,9 @@
 
 namespace libdnf {
 
-struct Dependency;
+class Dependency;
 
-struct DependencyContainer
+class DependencyContainer
 {
 public:
     DependencyContainer(const DependencyContainer &src);
