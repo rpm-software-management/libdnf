905037217da5af84a41cd51ad7e823d848f95ec4
Peter Kjellerstedt <pkj@axis.com>
2020-05-03 22:40:39 +0200

    Use single-quotes around string literals used in SQL statements
    If sqlite is built with -DSQLITE_DQS=0 in accordance with
    https://sqlite.org/quirks.html#dblquote, migration to version 1.2 of the
    history database would fail with:
    
      History database cannot be created: /var/lib/dnf/history.sqlite.
      Error: SQLite error on ":memory:": Executing an SQL statement failed:
      no such column: 1.2
    
    Closes: #951
    Approved by: jrohel

--- libdnf/transaction/sql/migrate_tables_1_2.sql
+++ libdnf/transaction/sql/migrate_tables_1_2.sql
@@ -1,9 +1,9 @@
 R"**(
 BEGIN TRANSACTION;
     ALTER TABLE trans
-        ADD comment TEXT DEFAULT "";
+        ADD comment TEXT DEFAULT '';
     UPDATE config
-        SET value = "1.2"
+        SET value = '1.2'
         WHERE key = 'version';
 COMMIT;
 )**"
