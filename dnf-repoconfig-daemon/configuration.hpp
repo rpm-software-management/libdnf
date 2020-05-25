7a3b43bd73aeec4b8a0365044c554e3e728d7a60
Marek Blaha <mblaha@redhat.com>
2020-05-25 08:59:44 +0200

    Add missing licenses

--- dnf-repoconfig-daemon/configuration.hpp
+++ dnf-repoconfig-daemon/configuration.hpp
@@ -1,25 +1,23 @@
 /*
  * Copyright (C) 2020 Red Hat, Inc.
  *
- * Licensed under the GNU Lesser General Public License Version 2.1
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-#ifndef DNFDAEMON_CONFIGURATION_HPP
-#define DNFDAEMON_CONFIGURATION_HPP
+#ifndef DNF_REPOCONFIG_DAEMON_CONFIGURATION_HPP
+#define DNF_REPOCONFIG_DAEMON_CONFIGURATION_HPP
 
 #include "../libdnf/conf/ConfigMain.hpp"
 #include "../libdnf/conf/ConfigParser.hpp"
