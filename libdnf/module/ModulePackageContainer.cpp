3b1f0a8537c319a8fa358c5803a130a703f153e4
Jaroslav Mracek <jmracek@redhat.com>
2020-08-11 14:26:19 +0200

    Adjust modular solver to new context type
    The new context allow us to install only the best modular candidate and
    not a candidate for each version.
    
    The change also allows to have broken the latest.

--- libdnf/module/ModulePackageContainer.cpp
+++ libdnf/module/ModulePackageContainer.cpp
@@ -49,6 +49,28 @@ extern "C" {
 #include "modulemd/ModuleMetadata.hpp"
 #include "modulemd/ModuleProfile.hpp"
 
+
+namespace {
+
+/// Requires resolved goal
+/// Takes listInstalls() from goal and keep solvables with the solvable-name (<name>:<stream>:<context>) in query 
+void goal2name_query(libdnf::Goal & goal, libdnf::Query & query)
+{
+    auto pool = dnf_sack_get_pool(goal.getSack());
+    auto installList = goal.listInstalls();
+    std::vector<const char *> module_names;
+    Id id = -1;
+    while ((id = installList.next(id)) != -1) {
+        Solvable * s = pool_id2solvable(pool, id);
+        const char * name = pool_id2str(pool, s->name);
+        module_names.push_back(name);
+    }
+    module_names.push_back(nullptr);
+    query.addFilter(HY_PKG_NAME, HY_EQ, module_names.data());
+}
+
+}
+
 namespace std {
 
 template<>
@@ -629,14 +651,14 @@ ModulePackageContainer::Impl::moduleSolve(const std::vector<ModulePackage *> & m
     for (const auto &module : modules) {
         std::ostringstream ss;
         auto name = module->getName();
-        ss << "module(" << name << ":" << module->getStream() << ":" << module->getVersion() << ")";
+        ss << "module(" << name << ":" << module->getStream() << ")";
         Selector selector(moduleSack);
         bool optional = persistor->getState(name) == ModuleState::DEFAULT;
         selector.set(HY_PKG_PROVIDES, HY_EQ, ss.str().c_str());
         goal.install(&selector, optional);
         goalWeak.install(&selector, true);
     }
-    auto ret = goal.run(DNF_IGNORE_WEAK);
+    auto ret = goal.run(static_cast<DnfGoalActions>(DNF_IGNORE_WEAK | DNF_FORCE_BEST));
     if (debugSolver) {
         goal.writeDebugdata("debugdata/modules");
     }
@@ -644,7 +666,7 @@ ModulePackageContainer::Impl::moduleSolve(const std::vector<ModulePackage *> & m
     auto problemType = ModulePackageContainer::ModuleErrorType::NO_ERROR;
     if (ret) {
         problems = goal.describeAllProblemRules(false);
-        ret = goal.run(DNF_NONE);
+        ret = goal.run(DNF_FORCE_BEST);
         if (ret) {
             // Conflicting modules has to be removed otherwice it could result than one of them will
             // be active
@@ -658,14 +680,20 @@ ModulePackageContainer::Impl::moduleSolve(const std::vector<ModulePackage *> & m
                 activatedModules.reset();
             } else {
                 problemType = ModulePackageContainer::ModuleErrorType::ERROR;
-                activatedModules.reset(new PackageSet(std::move(goalWeak.listInstalls())));
+                Query query(moduleSack, Query::ExcludeFlags::IGNORE_EXCLUDES);
+                goal2name_query(goalWeak, query);
+                activatedModules.reset(new PackageSet(*query.runSet()));
             }
         } else {
             problemType = ModulePackageContainer::ModuleErrorType::ERROR_IN_DEFAULTS;
-            activatedModules.reset(new PackageSet(std::move(goal.listInstalls())));
+            Query query(moduleSack, Query::ExcludeFlags::IGNORE_EXCLUDES);
+            goal2name_query(goal, query);
+            activatedModules.reset(new PackageSet(*query.runSet()));
         }
     } else {
-        activatedModules.reset(new PackageSet(std::move(goal.listInstalls())));
+        Query query(moduleSack, Query::ExcludeFlags::IGNORE_EXCLUDES);
+        goal2name_query(goal, query);
+        activatedModules.reset(new PackageSet(*query.runSet()));
     }
     return make_pair(problems, problemType);
 }
@@ -707,11 +735,13 @@ ModulePackageContainer::query(std::string name, std::string stream, std::string 
     query.available();
     std::ostringstream ss;
     ss << stringFormater(name) << ":" << stringFormater(stream);
-    ss << ":" << stringFormater(version) << ":";
-    ss << stringFormater(context);
+    ss << ":" << stringFormater(context);
     query.addFilter(HY_PKG_NAME, HY_GLOB, ss.str().c_str());
     if (!arch.empty()) {
         query.addFilter(HY_PKG_ARCH, HY_GLOB, arch.c_str());
+    }
+    if (!version.empty()) {
+        query.addFilter(HY_PKG_VERSION, HY_GLOB, version.c_str());
     }
     auto pset = query.runSet();
     Id moduleId = -1;
