be415f051a9e049469d884f6cf17bd1c3f217829
Dmitry Antipov <dantipov@cloudlinux.com>
2021-04-05 07:59:32 +0300

    Fix check whether the subkey can be used for signing

--- libdnf/repo/Repo.cpp
+++ libdnf/repo/Repo.cpp
@@ -726,7 +726,7 @@ static std::vector<Key> rawkey2infos(int fd) {
 
         // _extract_signing_subkey
         auto subkey = key->subkeys;
-        while (subkey && !key->subkeys->can_sign) {
+        while (subkey && !subkey->can_sign) {
             subkey = subkey->next;
         }
         if (subkey)
