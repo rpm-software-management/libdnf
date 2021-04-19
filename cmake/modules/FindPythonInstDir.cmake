f1cffbfb9f338da827e233c2d8ac3a25a6a59a69
Aleš Matěj <amatej@redhat.com>
2021-04-19 21:26:23 +0200

    Remove distutils which are deprecated in python3.10

--- cmake/modules/FindPythonInstDir.cmake
+++ cmake/modules/FindPythonInstDir.cmake
@@ -1,6 +1,6 @@
 EXECUTE_PROCESS(COMMAND ${PYTHON_EXECUTABLE} -c "
 from sys import stdout
-from distutils import sysconfig
-path=sysconfig.get_python_lib(True, prefix='${CMAKE_INSTALL_PREFIX}')
+from sysconfig import get_path
+path=get_path(name='platlib', vars={'platbase':'${CMAKE_INSTALL_PREFIX}'})
 stdout.write(path)"
 OUTPUT_VARIABLE PYTHON_INSTALL_DIR)
