89b931d8036499af18697332e16fe66955fdcee0
Evan Goode <mail@evangoo.de>
2024-05-07 16:28:59 +0000

    Add tests for shell-style variable expansion

--- tests/libdnf/conf/ConfigParserTest.cpp
+++ tests/libdnf/conf/ConfigParserTest.cpp
@@ -30,4 +30,21 @@ void ConfigParserTest::testConfigParserReleasever()
         libdnf::ConfigParser::substitute(text, substitutions);
         CPPUNIT_ASSERT(text == "major: , minor: ");
     }
+    {
+        std::map<std::string, std::string> substitutions = {
+            {"var1", "value123"},
+            {"var2", "456"},
+        };
+        std::string text = "foo$var1-bar";
+        libdnf::ConfigParser::substitute(text, substitutions);
+        CPPUNIT_ASSERT(text == "foovalue123-bar");
+
+        text = "${var1:+alternate}-${unset:-default}-${nn:+n${nn:-${nnn:}";
+        libdnf::ConfigParser::substitute(text, substitutions);
+        CPPUNIT_ASSERT(text == "alternate-default-${nn:+n${nn:-${nnn:}");
+
+        text = "${unset:-${var1:+${var2:+$var2}}}";
+        libdnf::ConfigParser::substitute(text, substitutions);
+        CPPUNIT_ASSERT(text == "456");
+    }
 }
