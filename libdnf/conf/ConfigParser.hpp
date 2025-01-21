7c8a76d9a1e64c368e0463f5294e770df6c454d7
Evan Goode <mail@evangoo.de>
2025-01-21 19:19:06 +0000

    ConfigParser: make splitReleasever public

--- libdnf/conf/ConfigParser.hpp
+++ libdnf/conf/ConfigParser.hpp
@@ -143,6 +143,7 @@ public:
     std::string & getHeader() noexcept;
     const Container & getData() const noexcept;
     Container & getData() noexcept;
+    static std::pair<std::string, std::string> splitReleasever(const std::string & releasever);
 
 private:
     std::map<std::string, std::string> substitutions;
@@ -162,8 +163,6 @@ private:
     static std::pair<std::string, size_t> substitute_expression(const std::string & text,
         const std::map<std::string, std::string> & substitutions,
         unsigned int depth);
-
-    static std::tuple<std::string, std::string> split_releasever(const std::string & releasever);
 };
 
 inline void ConfigParser::setSubstitutions(const std::map<std::string, std::string> & substitutions)
