rpm-software-management/libdnf

commit a067e647b17d6e66b6bce024be091ab14cae4daf

spec: Consistently use CMake RPM macros
Fedora is going to move from cmake with make to cmake with ninja
<https://fedoraproject.org/wiki/Changes/CMake_ninja_default>.
That would break building an RPM package from our spec file because
the new %cmake macro won't produce Makefiles:

    + /usr/bin/make -O -j4 V=1 VERBOSE=1
    make: *** No targets specified and no makefile found.  Stop.

The fix is to use CMake RPM macros for everything, especially not
executing make manually.

I tested this fix with old (make-based) and new (ninja-based)
cmake-rpm-macros package successfully.

Resolve: https://bugzilla.redhat.com/show_bug.cgi?id=2381038

libdnf.spec
@@ -204,7 +204,7 @@ pushd build-py2
   %endif
   %cmake -DPYTHON_DESIRED:FILEPATH=%{__python2} -DWITH_MAN=OFF ../ %{!?with_zchunk:-DWITH_ZCHUNK=OFF} %{!?with_valgrind:-DDISABLE_VALGRIND=1} %{_cmake_opts} -DLIBDNF_MAJOR_VERSION=%{libdnf_major_version} -DLIBDNF_MINOR_VERSION=%{libdnf_minor_version} -DLIBDNF_MICRO_VERSION=%{libdnf_micro_version} \
     -DWITH_SANITIZERS=%{?with_sanitizers:ON}%{!?with_sanitizers:OFF}
-  %make_build
+  %cmake_build
 popd
 %endif
 # endif with python2
@@ -218,28 +218,28 @@ pushd build-py3
   %endif
   %cmake -DPYTHON_DESIRED:FILEPATH=%{__python3} -DWITH_GIR=0 -DWITH_MAN=0 -Dgtkdoc=0 ../ %{!?with_zchunk:-DWITH_ZCHUNK=OFF} %{!?with_valgrind:-DDISABLE_VALGRIND=1} %{_cmake_opts} -DLIBDNF_MAJOR_VERSION=%{libdnf_major_version} -DLIBDNF_MINOR_VERSION=%{libdnf_minor_version} -DLIBDNF_MICRO_VERSION=%{libdnf_micro_version} \
     -DWITH_SANITIZERS=%{?with_sanitizers:ON}%{!?with_sanitizers:OFF}
-  %make_build
+  %cmake_build
 popd
 %endif
 
 %check
 %if %{with python2}
 pushd build-py2
-  make ARGS="-V" test
+  %ctest -V
 popd
 %endif
 %if %{with python3}
 # If we didn't run the general tests yet, do it now.
 %if %{without python2}
 pushd build-py3
-  make ARGS="-V" test
+  %ctest -V
 popd
 %else
 # Otherwise, run just the Python tests, not all of
 # them, since we have coverage of the core from the
 # first build
 pushd build-py3/python/hawkey/tests
-  make ARGS="-V" test
+  %ctest -V
 popd
 %endif
 %endif
@@ -247,12 +247,12 @@ popd
 %install
 %if %{with python2}
 pushd build-py2
-  %make_install
+  %cmake_install
 popd
 %endif
 %if %{with python3}
 pushd build-py3
-  %make_install
+  %cmake_install
 popd
 %endif
 

commit 0d63fdfbf5e46f993750de97ec9fa2682a5585f9

spec: Work around a bug in ctest macro on RHEL 9
Building the RPM package on RHEL 9 failed like this:

    error: /builddir/build/originals/libdnf.spec: line 238: Unknown option V in ctest(:-:)

The failure was introduced with commit
a067e647b17d6e66b6bce024be091ab14cae4daf ("spec: Consistently use CMake
RPM macros"). The cause is a bug in a defintion of the ctest macro
provided with cmake-rpm-macros-3.26.5-2.el9.noarch by RHEL 9
<https://issues.redhat.com/browse/RHEL-120543>.

This patch works around the bug by redefining the macro while accepting
any options.

libdnf.spec
@@ -226,6 +226,10 @@ popd
 %endif
 
 %check
+%if 0%{?rhel} == 9 && %{defined ctest}
+# Work around broken passing options to ctest macro, RHEL-120543
+%global ctest(-) %{expand:%{macrobody:ctest}}
+%endif
 %if %{with python2}
 pushd build-py2
   %ctest -V