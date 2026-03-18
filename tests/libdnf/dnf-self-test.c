3892eefb77e95a4dd9113a727503bb4ce29eac2c
Petr Písař <ppisar@redhat.com>
2026-03-18 13:36:18 +0100

    Fix dnf_keyring_add_public_key() to add all keys from an ASCII-armored block
    Upstream commit: 21e58e48d73a0edba62510bac8d49392c5166ebb
    
    The function was used to import only the first public key OpenPGP
    certificate into an in-memory keyring.  That broke a scenario when
    multiple keys were serialized into a single ASCII-armored OpenPGP
    block in a file.
    
    This patch iterates over all public keys in an ASCII-armored OpenPGP
    block.
    
    If an error is encountered, it continues importing next keys, but at
    the end raises the error. This is to be able to skip unsupported keys
    and to mimic what rpmkeys tool does.
    
    This patch also adds a test into test_libdnf_main test suite. It does
    not add any half-negative test (ie. first packet corrupted, second
    packet valid) as I did not find a way of triggering that behaviour.
    
    Resolves: https://redhat.atlassian.net/browse/RHEL-156063

--- tests/libdnf/dnf-self-test.c
+++ tests/libdnf/dnf-self-test.c
@@ -233,6 +233,39 @@ dnf_keyring_add_public_key_invalid(void)
     g_assert_false(verification_passed);
 }
 
+/* Test importing two keys from a file with an ASCII-armored block with two
+ * public key OpenPGP packets. */
+static void
+dnf_keyring_add_public_key_two_packets(void)
+{
+    g_autofree gchar *key_filename = NULL;
+    g_autofree gchar *data_filename = NULL;
+    g_autofree gchar *rsa_signature_filename = NULL;
+    g_autofree gchar *edsa_signature_filename = NULL;
+    gboolean import_passed;
+    g_autoptr(GError) import_error = NULL;
+    gboolean verification_passed;
+
+    key_filename = dnf_test_get_filename("dnf_keyring_add_public_key/twopackets.pub");
+    data_filename = dnf_test_get_filename("dnf_keyring_add_public_key/input");
+    rsa_signature_filename = dnf_test_get_filename("dnf_keyring_add_public_key/input.rsa.sig");
+    edsa_signature_filename = dnf_test_get_filename("dnf_keyring_add_public_key/input.edsa.sig");
+
+    import_and_verify(&import_passed, &import_error, &verification_passed,
+            key_filename, data_filename, rsa_signature_filename);
+    g_assert_true(import_passed);
+    g_assert_no_error(import_error);
+    g_clear_error(&import_error);
+    g_assert_true(verification_passed);
+
+    import_and_verify(&import_passed, &import_error, &verification_passed,
+            key_filename, data_filename, edsa_signature_filename);
+    g_assert_true(import_passed);
+    g_assert_no_error(import_error);
+    g_clear_error(&import_error);
+    g_assert_true(verification_passed);
+}
+
 static void
 dnf_lock_func(void)
 {
@@ -1452,6 +1485,7 @@ main(int argc, char **argv)
     g_test_add_func("/libdnf/context{cache-clean-check}", dnf_context_cache_clean_check_func);
     g_test_add_func("/libdnf/dnf_keyring_add_public_key[valid]", dnf_keyring_add_public_key_valid);
     g_test_add_func("/libdnf/dnf_keyring_add_public_key[invalid]", dnf_keyring_add_public_key_invalid);
+    g_test_add_func("/libdnf/dnf_keyring_add_public_key[two-packets]", dnf_keyring_add_public_key_two_packets);
     g_test_add_func("/libdnf/lock", dnf_lock_func);
     g_test_add_func("/libdnf/lock[threads]", dnf_lock_threads_func);
     g_test_add_func("/libdnf/split_releasever", dnf_split_releasever_func);
